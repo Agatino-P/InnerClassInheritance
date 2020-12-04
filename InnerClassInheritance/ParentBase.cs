ee0650637f29ef8846359560c500ea2e9e061717
Agatino Pesce <a.pesce@mecal.com>
2020-12-04 08:23:28 +0100

    Add project files.

---
+++ InnerClassInheritance/ParentBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InnerClassInheritance
+{
+    public class ParentBase
+    {
+        protected  ChildBase _child;
+        public int N1 { get; set; }
+
+        public ParentBase()
+        {
+            _child = new ChildBase(this);
+        }
+
+        public virtual void Do()
+        {
+            _child.Do1();
+        }
+        protected class ChildBase
+        {
+            protected ParentBase _parent;
+            public ChildBase(ParentBase parent)
+            {
+                _parent = parent;
+            }
+            public virtual void Do1()
+            {
+                _parent.N1++;
+            }
+        }
+    }
+}
---
+++ InnerClassInheritance/ParentDerived.cs
@@ -0,0 +1,37 @@
+﻿namespace InnerClassInheritance
+{
+    public class ParentDerived : ParentBase
+    {
+        protected new ChildDerived _child;
+        public int N2 { get; set; }
+
+        public ParentDerived()
+        {
+             _child= new ChildDerived(this);
+        }
+        public override void Do()
+        {
+            base.Do();
+            _child.Do2();
+        }
+
+        protected class ChildDerived : ChildBase
+        {
+            private new ParentDerived _parent;
+
+            public ChildDerived(ParentDerived parent) : base(parent)
+            {
+                _parent = parent;
+            }
+
+            public override void Do1()
+            {
+                _parent.N1--;
+            }
+            public virtual void Do2()
+            {
+                _parent.N2--;
+            }
+        }
+    }
+}
